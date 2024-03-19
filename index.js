b708092007a23391263623983ccf3d7f44e2fc72
Sebastian Schmidt <2270806+jammsen@users.noreply.github.com>
2024-03-19 13:40:10 +0100

    changed behavior for data-generation

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-const { exec } = require('child_process');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
+const axios = require("axios");
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
   
-// Start the server and socket.io
-server.listen(3000, () => {
-console.log(`Server started on port ${port}`);
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
-  
+
+function callAPI() {
+    axios.get(`http://10.0.0.10:2091/getcommands`).then((response) => {
+        io.emit('nvidia-smi', response.data.nvidia_smi);
+        io.emit('htop', response.data.htop);
+    }).catch((error) => {
+        console.error("Error making API request:", error);
+    });
+}
 
 setInterval(() => {
-    const ns_command = 'nvidia-smi | aha --black --line-fix';
-    exec(ns_command, (error, stdout, stderr) => {
-        if (error) {
-            io.emit('nvidia-smi', error);
-        } else {
-            io.emit('nvidia-smi', stdout);
-        }
-    });
-    const htop_command= 'echo q | htop | aha --black --line-fix';
-    exec(htop_command, (error, stdout, stderr) => {
-        if (error) {
-            io.emit('htop', error);
-        } else {
-            io.emit('htop', stdout);
-        }
-    });
-}, 500);
+    callAPI();
+}, 1000);
